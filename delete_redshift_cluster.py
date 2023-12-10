1bc0a69f9302b5fd5eb92774602a7f6b1b28b365
Michal Chacinski <mpchacinski@gmail.com>
2023-12-10 16:25:52 +0100

    added lines to also remove role when deleting redshift cluster

--- delete_redshift_cluster.py
+++ delete_redshift_cluster.py
@@ -10,6 +10,7 @@ aws_config.read_file(open('aws_auth.cfg'))
 KEY                    = aws_config.get('AWS','KEY')
 SECRET                 = aws_config.get('AWS','SECRET')
 DWH_CLUSTER_IDENTIFIER = dwh_config.get('DWH','DWH_CLUSTER_IDENTIFIER')
+DWH_IAM_ROLE_NAME      = dwh_config.get("DWH", "DWH_IAM_ROLE_NAME")
 
 redshift = boto3.client("redshift",
                        region_name="us-east-1",
@@ -17,8 +18,16 @@ redshift = boto3.client("redshift",
                        aws_secret_access_key=SECRET
                        )
 
+iam = boto3.client("iam",aws_access_key_id=KEY,
+                     aws_secret_access_key=SECRET,
+                     region_name="us-east-1"
+                  )
+
 redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
 
 cluster_spec = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
 cluster_status = cluster_spec["ClusterStatus"]
 print(cluster_status)
+
+iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
