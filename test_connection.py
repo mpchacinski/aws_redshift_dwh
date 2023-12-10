db2eec960acc70c9798cdd3da1ff60d217f6d97c
Michal Chacinski <mpchacinski@gmail.com>
2023-12-10 16:35:01 +0100

    small changes to test_connection file

--- test_connection.py
+++ test_connection.py
@@ -18,16 +18,16 @@ redshift = boto3.client("redshift",
                        aws_secret_access_key=SECRET
                        )
 
-cluster_spec = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+cluster_spec = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)["Clusters"][0]
 
-DWH_ENDPOINT = cluster_spec['Endpoint']['Address']
+DWH_ENDPOINT = cluster_spec["Endpoint"]["Address"]
 
 dwh_config.set("CLUSTER", "HOST", DWH_ENDPOINT)
 
-with open('dwh.cfg') as configfile:
+with open("dwh.cfg", "w") as configfile:
     dwh_config.write(configfile)
 
-conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*dwh_config['CLUSTER'].values()))
+conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*dwh_config["CLUSTER"].values()))
 cur = conn.cursor()
 
 cur.execute("SELECT CURRENT_DATE")
