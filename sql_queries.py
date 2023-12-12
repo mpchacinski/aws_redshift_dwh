598a30cf685cec8e01b23bf76ff6eff1bdd5e41f
Michal Chacinski <mpchacinski@gmail.com>
2023-12-12 11:15:08 +0100

    another star schema tables definitions created

--- sql_queries.py
+++ sql_queries.py
@@ -5,14 +5,18 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
+DWH_ROLE_ARN = config.get('IAM_ROLE','arn')
+LOG_JSON_PATH = config.get("S3", "log_jsonpath")
+LOG_DATA = config.get("S3", "log_data")
+
 # DROP TABLES
 
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
-songplay_table_drop = "DROP TABLE IF EXISTS songplay;"
-user_table_drop = "DROP TABLE IF EXISTS user;"
-song_table_drop = "DROP TABLE IF EXISTS song;"
-artist_table_drop = "DROP TABLE IF EXISTS artist;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
@@ -21,7 +25,7 @@ staging_events_table_create= ("""
 CREATE TABLE staging_events (
     artist varchar(150),
     auth varchar(20),
-    first_name varchar(300),
+    first_name varchar(30),
     gender varchar(1),
     item_in_session integer,
     last_name varchar(30),
@@ -44,45 +48,80 @@ staging_songs_table_create = ("""
 CREATE TABLE staging_songs (
     num_songs integer,
     artist_id varchar(20),
-    artist_latitude numeric,
-    artist_longitude numeric,
+    artist_latitude numeric(8, 6),
+    artist_longitude numeric(9, 6),
     artist_location varchar(150),
     artist_name varchar(150),
     song_id varchar(20),
-    title varchar(150),
+    title varchar(200),
     duration numeric(10, 4),
     year integer
+    );
 """)
 
 songplay_table_create = ("""
-CREATE TABLE songplay (
+CREATE TABLE songplays (
     songplay_id identity(1,1) PRIMARY KEY,
     start_time timestamp NOT NULL,
     user_id integer NOT NULL,
     level varchar(10),
-    song_id integer NOT NULL,
-    artist_id integer,
+    song_id varchar(20),
+    artist_id varchar(20),
     session_id integer,
     location varchar(150),
     user_agent(150)
+    );
 """)
 
 user_table_create = ("""
+CREATE TABLE users (
+    user_id integer PRIMARY KEY,
+    first_name varchar(30),
+    last_name varchar(30),
+    gender varchar(1),
+    level varchar(10)
+    );
 """)
 
 song_table_create = ("""
+CREATE TABLE songs (
+    song_id varchar(20) PRIMARY KEY,
+    title varchar(200),
+    artist_id varchar(20),
+    year integer,
+    duration numeric(10, 4)
+    );
 """)
 
 artist_table_create = ("""
+CREATE TABLE artists (
+   artist_id varchar(20) PRIMARY KEY,
+   name varchar(150),
+   location varchar(150),
+   latitude numeric(8, 6),
+   longitude numeric(9, 6)
+   );
 """)
 
 time_table_create = ("""
+CREATE TABLE time (
+    start_time timestamp PRIMARY KEY,
+    hour integer,
+    day integer,
+    week integer,
+    month integer,
+    year integer,
+    weekday integer
+    );
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+copy staging_events from {}
+credentials 'aws_iam_role={}'
+format as json {} region 'us-west-2';
+""").format(LOG_DATA, DWH_ROLE_ARN, LOG_JSON_PATH)
 
 staging_songs_copy = ("""
 """).format()
